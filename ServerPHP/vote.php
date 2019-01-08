47a4507cb78f94edcb2b0a452e0f295287423f81
Kamchai Boonsri <41858432+Kamchai-N@users.noreply.github.com>
2019-01-08 11:04:51 +0700

    Update vote.php

--- ServerPHP/vote.php
+++ ServerPHP/vote.php
@@ -1,6 +1,6 @@
 <?php   
     session_start();
-    $con = mysqli_connect('localhost','root','123456789','web_vote');
+    $con = mysqli_connect('XXXXX','XXXXX','XXXXX','web_vote');
     if(!$con){
         exit("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
     }
