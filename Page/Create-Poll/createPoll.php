3b10708b785ec96b640811a8b15e14a5b591ed4b
Kamchai Boonsri <41858432+Kamchai-N@users.noreply.github.com>
2019-01-08 11:18:45 +0700

    Update createPoll.php

--- Page/Create-Poll/createPoll.php
+++ Page/Create-Poll/createPoll.php
@@ -44,7 +44,7 @@ session_start();
     </script>
     <?php
         if($_POST){
-            $con = mysqli_connect('localhost','root','123456789','web_vote');
+            $con = mysqli_connect('XXXXX','XXXXX','XXXXX','web_vote');
             if(!$con){
                 exit("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
             }
